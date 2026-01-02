bceb3a893e9e3bd0bb986b6fb28a2c8a983c726c
basil medici <bme146158@stud.gibb.ch>
2026-01-02 22:56:40 +0100

    [new] Created a config file and  read config file

--- wellcome.py
+++ wellcome.py
@@ -1,1 +1,9 @@
-print ("Happy new year 2026, lets Go!")
+import json
+
+def main():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    print("Hello", config.get("Name"))
+
+if __name__ == "__main__":
+   main()
