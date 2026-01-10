62c0e51732e13112839246d805e9ad636478902d
basil medici <basil.medici@hotmail.com>
2026-01-10 21:36:27 +0100

    [chnage] keep it simple stupide

--- wellcome.py
+++ wellcome.py
@@ -1,29 +1,13 @@
 import json
-from textual.app import App
-from textual.widgets import Input, RichLog
-from textual.containers import Vertical
 
-class WellcomeApp(App):
-    CSS = """
-    Screen {
-        background: green;
-        color: green;
-    }
-    """
-    def compose(self):
-        yield Vertical(
-            RichLog(id="output"),
-            Input(placeholder=">"),
-        )
-        def on_input_submitted(self, event: Input.Submitted) -> None:
-            log = self.query_one("#output", RichLog)
-            log.write(f" {event.value}")
-    
+
 def main():
     with open('config.json', 'r') as f:
         config = json.load(f)
     print("Hello", config.get("Name"))
+    input("Press enter to continue...")
+
 
 # small comment dont have time today
 if __name__ == "__main__":
-   WellcomeApp().run()
+    main()
