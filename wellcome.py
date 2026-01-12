a3e8c8b69a56aeb1ddbfaa5b8702756a0ebf1dab
basil medici <basil.medici@hotmail.com>
2026-01-12 22:16:12 +0100

    [change] added the goals for the year

--- wellcome.py
+++ wellcome.py
@@ -1,11 +1,19 @@
 import json
-
+import time
 
 def main():
     with open('config.json', 'r') as f:
         config = json.load(f)
     print("Hello", config.get("Name"))
-    input("Press enter to continue...")
+    time.sleep(3)
+    goals = config.get("My goals for this year")
+    print("Your Goals for this year are:")
+    time.sleep(2)
+    for goal in goals:
+        print(goal)
+        time.sleep(2)
+    time.sleep(3)
+    input("Press Enter to continue...")
 
 
 # small comment dont have time today
